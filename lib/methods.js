Urigo/Ionic-MeteorCLI-WhatsApp

Step 5.10: Add remove chat method

lib/methods.js
@@ -57,5 +57,24 @@ Meteor.methods({
     const chatId = Chats.insert(chat);
 
     return chatId;
+  },
+  removeChat(chatId) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-logged-in',
+        'Must be logged to create a chat.');
+    }
+
+    check(chatId, String);
+
+    const chat = Chats.findOne(chatId);
+
+    if (!chat || !_.include(chat.userIds, this.userId)) {
+      throw new Meteor.Error('chat-not-exists',
+        'Chat not exists');
+    }
+
+    Messages.remove({ chatId: chatId });
+
+    return Chats.remove({ _id: chatId });
   }
 });

Entrepreneurship-bootcamp-02/angular-meteor-whatsapp

lib/methods.js
@@ -58,5 +58,24 @@ Meteor.methods({
     const chatId = Chats.insert(chat);
 
     return chatId;
+  },
+  removeChat(chatId) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-logged-in',
+        'Must be logged to remove a chat.');
+    }
+
+    check(chatId, String);
+
+    const chat = Chats.findOne(chatId);
+
+    if (!chat || !_.include(chat.userIds, this.userId)) {
+      throw new Meteor.Error('chat-not-exists',
+        'Chat not exists');
+    }
+
+    Messages.remove({ chatId: chatId });
+
+    return Chats.remove({ _id: chatId });
   }
 });